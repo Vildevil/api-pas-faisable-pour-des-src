b65ddf1a029004ae22b4abf6e45f305a994c52a9
Apollinaire DEGREZ <blockshoooter@outlook.fr>
2024-06-20 15:31:23 +0200

    [ADD] TEST 0.0.2

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
-from security.auth import authenticate, identity
+from flask_sqlalchemy import SQLAlchemy
 from resources.coach import CoachResource
 from resources.player import PlayerResource
 from resources.teams import TeamResource
@@ -9,13 +9,20 @@ from resources.teams import TeamResource
 app = Flask(__name__)
 app.config.from_object('config.Config')
 
+db = SQLAlchemy(app)
 api = Api(app)
 jwt = JWTManager(app)
 
-# Add resources to the API
+# Ajouter les ressources à l'API
 api.add_resource(CoachResource, '/coaches/<int:id>', '/coaches')
 api.add_resource(PlayerResource, '/players/<int:id>', '/players')
 api.add_resource(TeamResource, '/teams/<int:id>', '/teams')
 
 if __name__ == '__main__':
+    from models.coach import Coach
+    from models.player import Player
+    from models.teams import Team
+
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
